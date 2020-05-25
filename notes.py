c06d403b9d2e9db48802d098ceffbbe83db2c174
Mariyan Zarev <mzare002@gold.ac.uk>
2020-05-25 15:55:24 +0100

    chapter 1

---
+++ notes.py
@@ -0,0 +1,17 @@
+def notes():
+  # first sim linear acceleration
+  # second sim sideways
+  # third sim rotation... 6DOF?
+  # haptics for controllers:
+  # https://hal.archives-ouvertes.fr/hal-01524537/document
+  # potential haptic vest: 
+  # http://www.korfx.com
+  # sim dashboard guthub:
+  # https://github.com/Alex-developer/OneHUD/tree/master/ProjectCars2
+  # READ/WRITE in game memory:
+  # https://www.youtube.com/watch?v=Mm3ZK3uAeuo
+  # find static memory addresses in games:
+  # https://www.youtube.com/watch?v=hfWOAFsYnFA
+  # game hacking website:
+  # https://www.dev798.com/archives/2262
+  pass
