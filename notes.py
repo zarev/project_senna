a9d09545fb44720844fd239107f45be609440045
Mariyan Zarev <mzare002@gold.ac.uk>
2020-05-26 14:58:31 +0100

    moar links

--- notes.py
+++ notes.py
@@ -12,6 +12,20 @@ def notes():
   # https://www.youtube.com/watch?v=Mm3ZK3uAeuo
   # find static memory addresses in games:
   # https://www.youtube.com/watch?v=hfWOAFsYnFA
+  # if nothing else works, more address fishing:
+  # https://www.youtube.com/watch?v=eU4UGVvQDME&feature=youtu.be
+  # forum that suggested above:
+  # https://forum.cheatengine.org/viewtopic.php?p=5743349&sid=21d9cea6a573769709636496dc7e5671
+  # moar blogs:
+  # https://www.cheatengine.org/forum/viewtopic.php?t=608724&sid=c0f8c32bec03a1f0a5f16db08e98121a
+  # pointer maps, if ALL else fails
+  # https://www.youtube.com/watch?v=nQ2F2iW80Fk&feature=youtu.be
   # game hacking website:
   # https://www.dev798.com/archives/2262
+  # to access memory address of pointers game needs to
+  # run CE or VEH with dbvm/trueplay disabled
+  # list of haptic suits(most bankrupt):
+  # https://en.wikipedia.org/wiki/Haptic_suit
+  # monster simulator: https://www.youtube.com/watch?v=hQLF06rwzVE
+  
   pass
