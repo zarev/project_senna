da6bca374701bc853ed7f023f07182f99d805900
Mariyan <mzarev1@gmail.com>
2020-05-28 12:34:57 +0100

    more notes

--- notes.py
+++ notes.py
@@ -1,7 +1,28 @@
 def notes():
-  # first sim linear acceleration
-  # second sim sideways
-  # third sim rotation... 6DOF?
+  # to access memory address of pointers game needs to
+  # run CE or VEH with dbvm/trueplay disabled
+  pass
+
+def game_plan():
+  # A: linear acceleration
+  # B: sideways
+  # C: rotation... 6DOF?
+
+  # 1: ---get acceleration data---
+  #  1.1: figure out how to base addresses
+  #   1.1.1: Find addresses for the 6 DOF
+  #   1.1.2: Find address for G-Force Value 
+  #  1.2: perform AOB scan to find static address for acceleration
+  #  1.3: script to print the data live
+  # 2: ---translate the data into a motion model---
+  #  2.1: start with linear only
+  #  2.2: add sideways
+  # 3: ---implement with the vest---
+  #  3.1: together with 2.1
+  pass
+
+def references():
+
   # haptics for controllers:
   # https://hal.archives-ouvertes.fr/hal-01524537/document
   # potential haptic vest: 
@@ -18,14 +39,13 @@ def notes():
   # https://forum.cheatengine.org/viewtopic.php?p=5743349&sid=21d9cea6a573769709636496dc7e5671
   # moar blogs:
   # https://www.cheatengine.org/forum/viewtopic.php?t=608724&sid=c0f8c32bec03a1f0a5f16db08e98121a
-  # pointer maps, if ALL else fails
-  # https://www.youtube.com/watch?v=nQ2F2iW80Fk&feature=youtu.be
+  # pointer map with 2 pointers:
+  # https://www.youtube.com/watch?v=nQ2F2iW80Fk
   # game hacking website:
   # https://www.dev798.com/archives/2262
-  # to access memory address of pointers game needs to
-  # run CE or VEH with dbvm/trueplay disabled
   # list of haptic suits(most bankrupt):
   # https://en.wikipedia.org/wiki/Haptic_suit
   # monster simulator: https://www.youtube.com/watch?v=hQLF06rwzVE
-  
+  # create AOB:
+  # https://www.youtube.com/watch?v=5GQ6nOi9oQ0
   pass
